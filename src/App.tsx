1eb791948d49e916bb7ddb63b0f78d1d3b3455df
Tony Xiao <tonyx.ca@gmail.com>
2022-03-21 11:10:53 +0800

    First release. Got it working

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+/*
+Input: 
+
+main.bean	32	Balance failed for Assets:Inventory:Fresh: expected 0.1 EDAMAME_KG != accumulated 1.0 EDAMAME_KG (0.9 too much)
+main.bean	33	Balance failed for Assets:Inventory:Fresh: expected 0.7 MUSHROOM_OYSTER_KG != accumulated 1.0 MUSHROOM_OYSTER_KG (0.3 too much)
+  
+Expect output:
+
+Assets:Inventory:Fresh  -0.9 EDAMAME_KG {}
+Assets:Inventory:Fresh: expected 0.7 MUSHROOM_OYSTER_KG != accumulated 1.0 MUSHROOM_OYSTER_KG (0.3 too much)
+
+Produced using: https://regexr.com/
+*/
+function assertionErrorToPaddingTransaction(errStr: string) {
+  const inventoryPostings = errStr
+    .split("\n")
+    .map((line) => {
+      const match = line.match(
+        /Balance failed for (.*?): expected .*?(\w+) \(([\.\d]+) too much/
+      );
+      if (!match) {
+        return "";
+      }
+      const [accountName, commodity, quantity] = match.slice(1);
+
+      const out = `  ${accountName}  -${quantity} ${commodity} {}`;
+
+      console.log(out);
+      return out;
+    })
+    .filter((l) => !!l)
+    .join("\n");
+
+  return [
+    `${new Date().toISOString().slice(0, 10)} * "Food cost"`,
+    inventoryPostings,
+    '  Expenses:FoodCost'
+  ].join("\n");
+}
 
 function App() {
+  const [input, setInput] = React.useState(`
+main.bean	32	Balance failed for Assets:Inventory:Fresh: expected 0.1 EDAMAME_KG != accumulated 1.0 EDAMAME_KG (0.9 too much)
+main.bean	33	Balance failed for Assets:Inventory:Fresh: expected 0.7 MUSHROOM_OYSTER_KG != accumulated 1.0 MUSHROOM_OYSTER_KG (0.3 too much)
+  `);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <label>
+            Input
+            <textarea
+              value={input}
+              onChange={(v) => setInput(v.currentTarget.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Output
+            <textarea
+              value={assertionErrorToPaddingTransaction(input)}
+            ></textarea>
+          </label>
+        </div>
       </header>
     </div>
   );
