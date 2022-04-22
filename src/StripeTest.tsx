4a2eda16b8ef6e425a8e3fcbb1fa12cc6d0d42ee
Tony Xiao <tonyx.ca@gmail.com>
2022-04-22 17:29:37 +0800

    [WIP] Stripe test 2

--- src/StripeTest.tsx
+++ src/StripeTest.tsx
@@ -7,6 +7,7 @@ import {
   Elements,
   PaymentElement,
   PaymentRequestButtonElement,
+  useElements,
   useStripe,
   // useStripe,
   // useElements,
@@ -59,7 +60,46 @@ export function PaymentRequestButtonForm() {
 }
 
 function PaymentForm() {
-  return <PaymentElement />;
+  const stripe = useStripe();
+  const elements = useElements();
+
+  return (
+    <form
+      onSubmit={async (event) => {
+        // We don't want to let default form submission happen here,
+        // which would refresh the page.
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+          // Stripe.js has not yet loaded.
+          // Make sure to disable form submission until Stripe.js has loaded.
+          return;
+        }
+
+        const { error } = await stripe.confirmPayment({
+          //`Elements` instance that was used to create the Payment Element
+          elements,
+          confirmParams: {
+            return_url: "https://6897-182-253-75-232.ngrok.io/stripe",
+          },
+        });
+
+        if (error) {
+          // This point will only be reached if there is an immediate error when
+          // confirming the payment. Show error to your customer (for example, payment
+          // details incomplete)
+          console.error(error.message);
+        } else {
+          // Your customer will be redirected to your `return_url`. For some payment
+          // methods like iDEAL, your customer will be redirected to an intermediate
+          // site first to authorize the payment, then redirected to the `return_url`.
+        }
+      }}
+    >
+      <PaymentElement />
+      <button disabled={!stripe}>Submit</button>
+    </form>
+  );
 }
 
 export function StripeTest() {
